d46d4084b03af43505e6c72271b1e2af90934b64
maxendro <98157983+maxendro@users.noreply.github.com>
2023-08-24 12:09:37 +0300

    Update hw9_set.py

--- hw9_set.py
+++ hw9_set.py
@@ -12,7 +12,7 @@ n = 1000
 try:
     producer = KafkaProducer(bootstrap_servers='vm-strmng-s-1.test.local:9092')
     # Выставляем локаль для faker
-    fake = Faker('ru_RU')
+    fake = Faker('en_US')
     #выставление весов параметра priority для использования в random.choice
     priority_list = ['0'] * int(n*0.75) + ['1'] * int(n*0.2) + ['2'] * int(n*0.05)
     for i in range(n):
@@ -25,8 +25,8 @@ try:
         # отправляем сообщение в очередь kafka
         producer.send('lab10.kuznetsov', value=message_json)
         print(message_json)
+    print("[INFO] В очередь ушло {} записей:".format(i + 1))
 except Exception as _ex:
       print("[ERR] Ошибка : ", _ex)
 finally:
-      print("[INFO] В очередь ушло {} записей:".format(i+1))
       producer.close()
