18b6c502e4ee9e85e7484d3abbe17d784d908cd9
maxendro <98157983+maxendro@users.noreply.github.com>
2023-08-31 17:32:38 +0300

    Update edit_pdf2.py

--- edit_pdf2.py
+++ edit_pdf2.py
@@ -1,19 +1,24 @@
 import aspose.pdf as ap
 import fitz
+import os
 
 # Имя pdf - файла для замены
 pdf_in = "pdf_in.pdf"
+# Временный файл с заменой, но watermark
 pdf_temp = "pdf_temp.pdf"
+# Итоговый файл
 pdf_out= "pdf_out.pdf"
 
-repl_from = "июля"
+# Что меняем
+repl_what = "июля"
+# На что меняем
 repl_to = "июня"
 
 # Загружаем PDF-документ
 document = ap.Document(pdf_in)
 
 # Создание экземпляра объекта TextFragmentAbsorber
-txtAbsorber = ap.text.TextFragmentAbsorber(repl_from)
+txtAbsorber = ap.text.TextFragmentAbsorber(repl_what)
 
 # Поиск текста
 document.pages.accept(txtAbsorber)
@@ -34,3 +39,6 @@ for page in doc:
     page.draw_rect([1,1,300,20],   color=None, fill=fitz.utils.getColor('white'), overlay=True, fill_opacity=1)
 # Save pdf
 doc.save(pdf_out)
+doc.close()
+# удаляем временный
+os.remove(pdf_temp)
