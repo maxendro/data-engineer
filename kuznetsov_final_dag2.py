a82d5adf694ed82443a481be095dc50c5f2c9f9d
maxendro <98157983+maxendro@users.noreply.github.com>
2023-10-11 17:31:44 +0300

    Update kuznetsov_final_dag2.py

--- kuznetsov_final_dag2.py
+++ kuznetsov_final_dag2.py
@@ -14,29 +14,11 @@ sql_drop_t = f"drop table if exists final_kuznetsov_warm cascade;"
 sql_create_t = f"CREATE TABLE final_kuznetsov_warm (user_id  int4,content_id int4,start_s timestamp,stop_s timestamp, channel_id int, region_id int) WITH (	appendonly=true, orientation=column,	compresstype=zstd,	compresslevel=1)DISTRIBUTED RANDOMLY;"
 sql_insert_t = f"insert into final_kuznetsov_warm select * from final_kuznetsov_ext e where e.start_s > date_trunc('day'::text, now() - '6 mons'::interval)";
 sql_drop_v = f"drop view if exists v_final_kuznetsov_hot;"
-sql_create_v = f"create or replace view v_final_kuznetsov_hot as \
- SELECT l.user_id, \
-    l.content_id, \
-    fkc.content_name,\
-    l.start_s,\
-    l.stop_s,\
-    fkch.channel_name,\
-    l.region_id,\
-    fkr.region_name,\
-    fkr.code\
-   FROM final_kuznetsov_warm l\
-     JOIN final_kuznetsov_content fkc ON l.content_id = fkc.content_id\
-     JOIN final_kuznetsov_regions fkr ON l.region_id = fkr.region_id\
-     JOIN final_kuznetsov_channels fkch ON l.channel_id = fkch.channel_id\
-where\
-	l.start_s::date between date_trunc('week',\
-	current_date)::date - 7 \
-          and date_trunc('week',\
-	current_date)::date;"
+sql_create_v = f"create or replace view v_final_kuznetsov_hot as SELECT l.user_id, l.content_id, fkc.content_name, l.start_s,  l.stop_s, fkch.channel_name, l.region_id, fkr.region_name, fkr.code  FROM final_kuznetsov_warm l JOIN final_kuznetsov_content fkc ON l.content_id = fkc.content_id JOIN final_kuznetsov_regions fkr ON l.region_id = fkr.region_id   JOIN final_kuznetsov_channels fkch ON l.channel_id = fkch.channel_id where l.start_s::date between date_trunc('week', current_date)::date - 7 and date_trunc('week',current_date)::date;"
 
 
 args = {'owner': 'kuznetsov',
-        'start_date': datetime(2023, 11, 10), # дата запуска
+        'start_date': datetime(2023, 10, 11), # дата запуска
         'retries': 3,    # количество попыток в случае аварии
         'retry_delay': timedelta(seconds=300)}  # время между попытками
 
